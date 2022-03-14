42a129ffb8ecf4c0fc89d42a9566273406792814
Chris Mathew <chrismathew05@gmail.com>
2022-03-14 18:07:15 -0400

    Added mock imports for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../../app/"))
-
+autodoc_mock_imports = ["config.json"]
 
 # -- Project information -----------------------------------------------------
 
